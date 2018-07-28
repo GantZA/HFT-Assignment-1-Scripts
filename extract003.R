96072ade5623d22a8d9bba37d4f9dcb4da53ef6f
Michael Gant <michael.j.g.za@gmail.com>
2018-07-28 10:09:34 +0200

    defined directory variable

--- extract003.R
+++ extract003.R
@@ -3,8 +3,8 @@ library(lubridate)
 con <- blpConnect() 
 ################################
 
-#################  Before Running make sure your directory and security variables are correct!   #######
-
+#Before Running make sure your directory and security variables are correct!   #######
+dir = "F:/data/"
 
 #################################
 security = c("CPI SJ Equity", "SBK SJ Equity", "SOL SJ Equity","CLS SJ Equity", "FSR SJ Equity", "SLM SJ Equity","MTN SJ Equity", 
@@ -17,7 +17,7 @@ StartTime = as.POSIXct("2017-06-1 08:00:00")    #### Doesnt seem to matter what 
 daily_sampled = list()
 for (i in 1:10){
   daily_sampled[[i]] = getBars(security = security[i], barInterval = 540, startTime = StartTime, con = con)
-  file_name = paste("F:/data/daily_sampled/", substr(security[i],1,3), ".csv", sep = "")
+  file_name = paste(dir,"daily_sampled/", substr(security[i],1,3), ".csv", sep = "")
   write.csv(daily_sampled[[i]], file_name, row.names = F)
 }
 
@@ -25,7 +25,7 @@ for (i in 1:10){
 StartTime = as.POSIXct("2018-01-28 05:00:00")
 
 for (i in 1:10){
-  file_name = paste("F:/data/intraday/trade_", substr(security[i],1,3), ".csv", sep = "")
+  file_name = paste(dir, "intraday/trade_", substr(security[i],1,3), ".csv", sep = "")
   temp = getTicks(security = security[i], eventType = c("TRADE"), startTime = StartTime, con = con)
   write.csv(temp, file_name, row.names = F)
 }
@@ -35,7 +35,7 @@ for (i in 1:10){
   EndTime = StartTime
   month(EndTime) = month(StartTime) + 1
   for (k in 1:6){
-    file_name = paste("F:/data/intraday/ask_","00", k,"_" , substr(security[i],1,3), ".csv", sep = "")
+    file_name = paste(dir,"intraday/ask_","00", k,"_" , substr(security[i],1,3), ".csv", sep = "")
     temp = getTicks(security = security[i], eventType = c("ASK"), startTime = StartTime, endTime = EndTime,  
                     con = con)
     write.csv(temp, file_name, row.names = F)
@@ -49,7 +49,7 @@ for (i in 1:10){
   EndTime = StartTime
   month(EndTime) = month(StartTime) + 1
   for (k in 1:6){
-    file_name = paste("F:/data/intraday/bid_","00", k,"_" , substr(security[i],1,3), ".csv", sep = "")
+    file_name = paste(dir, "intraday/bid_","00", k,"_" , substr(security[i],1,3), ".csv", sep = "")
     temp = getTicks(security = security[i], eventType = c("BID"), startTime = StartTime, endTime = EndTime,  
                     con = con)
     write.csv(temp, file_name, row.names = F)
